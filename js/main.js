07a84a1a4ac6966180c807e0f9bb0830d3994194
Kaushil <c0826985@mylambton.ca>
2022-04-14 18:54:20 -0400

    media queries added,loader done,navbar & loader js added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector("#menu-bar");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () =>{
+  navbar.classList.toggle("active")
+}
+function loader () {
+  document.querySelector(".loader-container").classList.add("fade-out")
+}
+function fadeOut(){
+  setInterval(loader , 3000)
+}
+window.onload = fadeOut ();
